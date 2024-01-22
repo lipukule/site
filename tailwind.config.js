5a3c7e3bfd0cb0384d67f4ae14d02d6b82be8fd5
Janet Blackquill <uhhadd@gmail.com>
2024-01-22 12:02:56 -0500

    sin: lipu li jo e nasin lukin sin

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  darkMode: 'media',
+  content: [
+    "./themes/**/layouts/**/*.html",
+    "./content/**/layouts/**/*.html",
+    "./layouts/**/*.html",
+    "./content/**/*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cream: '#f9f4ed',
+        'cream-red': '#ffeff5',
+        'cream-orange': '#ffeacb',
+        'dark-cream': '#2a2a2a',
+        'dark-cream-red': '#352724',
+        'dark-cream-orange': '#30291f',
+        'lojunu': '#ffa4c5',
+        'loje': '#ffa893',
+        'lojelo': '#ffb777',
+        'jelo': '#cac976',
+        'graso': '#8ed890',
+        'laso': '#66dfc2',
+        'blaso': '#5bddfd',
+        'lasewi': '#76d0ff',
+        'lasunu': '#b3beff',
+        'unu': '#fbadff',
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ]
+}
